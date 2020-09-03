25107822d7d8f06c75a33d8a048fc99001ecc9fd
Andre Beserra <andresouza@multconnect.com.br>
2020-09-03 00:26:23 -0300

    Updates

---
+++ precache-manifest.ae292ff319247978abc99841e4bd2e5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2f440d7616e548c2a6414452cf4b3e",
+    "url": "/challenge-contraktor/index.html"
+  },
+  {
+    "revision": "7c6ce34e02b81264a612",
+    "url": "/challenge-contraktor/static/css/main.19cd8f13.chunk.css"
+  },
+  {
+    "revision": "1504e08c0b4d53f8871a",
+    "url": "/challenge-contraktor/static/js/2.a8e13cfa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/challenge-contraktor/static/js/2.a8e13cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6ce34e02b81264a612",
+    "url": "/challenge-contraktor/static/js/main.c4fc7356.chunk.js"
+  },
+  {
+    "revision": "2cf1c5a4c3e75d3b323f",
+    "url": "/challenge-contraktor/static/js/runtime-main.d3954476.js"
+  },
+  {
+    "revision": "d2e691bc4a2b696929172cb3d22ce8ba",
+    "url": "/challenge-contraktor/static/media/Nunito-Regular.d2e691bc.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.23dcc66b8efaf1b011b08aac134cc99d.js"
+  "/challenge-contraktor/precache-manifest.ae292ff319247978abc99841e4bd2e5e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/challenge-contraktor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
