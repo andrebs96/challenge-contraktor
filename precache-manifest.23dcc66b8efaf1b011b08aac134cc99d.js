b93ccb6c38df132437875a5f829f4edf322b0a6e
Andre Beserra <andresouza@multconnect.com.br>
2020-09-03 00:18:18 -0300

    Updates

---
+++ precache-manifest.23dcc66b8efaf1b011b08aac134cc99d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf648fc56b8eb65af68eec4da5baaf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "402ad5a695119ecb72d7",
+    "url": "/static/css/main.d8d0ed65.chunk.css"
+  },
+  {
+    "revision": "1504e08c0b4d53f8871a",
+    "url": "/static/js/2.a8e13cfa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a8e13cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "402ad5a695119ecb72d7",
+    "url": "/static/js/main.9b722a37.chunk.js"
+  },
+  {
+    "revision": "ab21bf44ca1b12d90053",
+    "url": "/static/js/runtime-main.1e6069e7.js"
+  },
+  {
+    "revision": "d2e691bc4a2b696929172cb3d22ce8ba",
+    "url": "/static/media/Nunito-Regular.d2e691bc.ttf"
+  }
+]);
